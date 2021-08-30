f67e3831881207f74a2e5ffdde54526d88238f33
hyzen <rajat@quoality.com>
2021-08-30 06:07:50 +0530

    passport stratogy Implemented

--- api/auth.route.js
+++ api/auth.route.js
@@ -1,37 +1,94 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jsonwt = require("jsonwebtoken");
+const passport = require("passport");
 
-// Load Controllers
-const {
-  registerController,
-  activationController,
-  signinController,
-  forgotPasswordController,
-  resetPasswordController,
-  googleController,
-} = require("../controllers/auth.controller");
+router.get("/", (req, res) => res.json({ Test: "Auth is being tested" }));
 
-const {
-  validSign,
-  validLogin,
-  forgotPasswordValidator,
-  resetPasswordValidator,
-} = require("../helpers/valid");
+const Person = require("../models/Person");
 
-router.post("/register", validSign, registerController);
+router.post("/register", (req, res) => {
+  Person.findOne({ email: req.body.email })
+    .then(person => {
+      if (person) {
+        return res
+          .status(400)
+          .json({ emailerror: "Email is already registered in our system " });
+      } else {
+        const newPerson = new Person({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        });
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newPerson.password, salt, (err, hash) => {
+            if (err) throw err;
+            newPerson.password = hash;
+            newPerson
+              .save()
+              .then(person => res.json(person))
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    })
+    .catch(err => console.log(err));
+});
 
-router.post("/login", validLogin, signinController);
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-router.post("/activation", activationController);
+  Person.findOne({ email })
+    .then(person => {
+      if (!person) {
+        return res
+          .status(404)
+          .json({ emailerror: "User not found with this email" });
+      }
+      bcrypt
+        .compare(password, person.password)
+        .then(isCorrect => {
+          if (isCorrect) {
+            // res.json({ success: "Authentication Succesfull" });
+            const payload = {
+              id: person.id,
+              name: person.name,
+              email: person.email,
+            };
+            jsonwt.sign(
+              payload,
+              process.env.KEY,
+              { expiresIn: 60 * 60 },
+              (err, token) => {
+                if (err) throw err;
+                res.json({
+                  success: true,
+                  token: "Bearer " + token,
+                });
+              }
+            );
+          } else {
+            res.status(400).json({ passworderror: " Password is not correct" });
+          }
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+});
 
-// forgot reset password
-router.put(
-  "/forgotpassword",
-  forgotPasswordValidator,
-  forgotPasswordController
+router.get(
+  "/profile",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email,
+      profilepic: req.user.profilepic,
+    });
+  }
 );
-router.put("/resetpassword", resetPasswordValidator, resetPasswordController);
 
-// Google
-router.post("/googlelogin", googleController);
 module.exports = router;
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const passport = require("passport");
 const connectDB = require("./config/db");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 
 // Config dotev...
 require("dotenv").config({
@@ -15,36 +14,26 @@ const app = express();
 connectDB();
 
 // body parser
-app.use(bodyParser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 
-// Load routes
-const authRouter = require("./api/auth.route");
-const newsRouter = require("./api/news.route");
-const weatherRouter = require("./api/weather.route");
+//Load Routes
+const auth = require("./api/auth.route");
+const news = require("./api/news.route.js");
+const weather = require("./api/weather.route");
 
-// Dev Logging Middleware
-if (process.env.NODE_ENV === "development") {
-  app.use(cors());
-  app.use(morgan("dev"));
-}
+app.use(passport.initialize());
 
-// Use Routes
-app.use("/", res => {
-  console.log("working");
-});
-app.use("/api", authRouter);
-app.use("/api/news", newsRouter);
-app.use("/api/weather", weatherRouter);
+require("./strategies/jsonwtStrategy")(passport);
 
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    msg: "Page not found",
-  });
+app.get("/", (req, res) => {
+  res.send("Working");
 });
 
-const PORT = process.env.PORT || 5000;
+app.use("/api/auth", auth);
+// app.use("/api/news", news);
+// app.use("/api/weather", weather);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Running`));
